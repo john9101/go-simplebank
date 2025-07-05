3a4a99acbfa2dbd6bde8b56ba4df31189cd6289a
john9101 <21130319@st.hcmuaf.edu.vn>
2025-07-05 14:49:17 +0700

    extract metadata information from grpc

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -45,12 +45,13 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "failed to create refresh token")
 	}
 
+	metadate := server.extractMetadata(&ctx)
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    metadate.UserAgent,
+		ClientIp:     metadate.ClientIP,
 		IsBlocked:    false,
 		ExpiresAt:    refreshPayload.ExpiredAt,
 	})
